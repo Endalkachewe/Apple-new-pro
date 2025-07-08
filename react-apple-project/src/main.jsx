c9f9d7f3441c3d5aa1115f50264ccf9e1d5886b3
Endalkachewe <133228700+Endalkachewe@users.noreply.github.com>
2025-07-07 17:06:00 -0800

    updated

--- react-apple-project/src/main.jsx
+++ react-apple-project/src/main.jsx
@@ -1,9 +1,19 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import "./Resources/css/bootstrap.css"
+// import "./Resources/css/styles.css"
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+// Import libraries and CSS here, NOT in index.html
+import 'bootstrap/dist/css/bootstrap.min.css'; // The correct way to add Bootstrap CSS
+import './Resources/css/styles.css' // Your custom styles
 import './index.css'
+
 import App from './App.jsx'
-import "./Resources/css/bootstrap.css"
-import "./Resources/css/styles.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
